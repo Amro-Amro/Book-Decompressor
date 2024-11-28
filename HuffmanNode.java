ac46843bd1493ad28a65debc5dfb335677aefe1e
Amro Abu-atieh <160271190+Amro-Amro@users.noreply.github.com>
2024-11-28 16:06:09 -0600

    Update HuffmanNode.java

--- HuffmanNode.java
+++ HuffmanNode.java
@@ -1,1 +1,55 @@
+package student;
 
+import provided.BinarySequence;
+
+public class HuffmanNode {
+    
+    private HuffmanNode zero; //left
+    private HuffmanNode one; //right
+    private Character data;
+
+    public HuffmanNode(HuffmanNode zero, HuffmanNode one) {
+        this.zero = zero;
+        this.one = one;
+        this.data = null;
+    }
+
+    public HuffmanNode(char data) {
+        this.data = data;
+        this.zero = null;
+        this.one = null;
+    }
+
+    public HuffmanNode getZero() {
+        return zero;
+    }
+
+    public void setZero(HuffmanNode zero) {
+        this.zero = zero;
+    }
+
+    public HuffmanNode getOne() {
+        return one;
+    }
+
+    public void setOne(HuffmanNode one) {
+        this.one = one;
+    }
+
+    public Character getData() {
+        return data;
+    }
+
+    public void setData(char data) {
+        this.data = data;
+    }
+
+    public boolean isLeaf() {
+        return zero == null && one == null;
+    }
+
+    public boolean isValidNode() {}
+
+    public boolean isValidTree() {}
+
+}
